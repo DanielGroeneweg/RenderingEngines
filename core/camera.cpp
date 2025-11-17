8bdfaca8baa1633c6f766a86003b1f4286b7e356
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-11-17 09:33:07 +0100

    inital comit

---
+++ core/camera.cpp
@@ -0,0 +1,32 @@
+﻿#include "camera.h"
+
+#include <ios>
+#include <glm/gtc/quaternion.hpp>
+
+void Camera::MoveCamera(glm::vec3 move) {
+    this->position += move;
+    this->view = glm::lookAt(this->position, this->position + this->forward, this->up);
+}
+void Camera::RotateCamera(glm::vec2 rotation) {
+    this->yaw = rotation.x;
+    this->pitch = rotation.y;
+    pitch = glm::clamp(pitch, -89.0f, 89.0f);
+    UpdateCameraVectors();
+
+    this->view = glm::lookAt(this->position, this->position + this->forward, this->up);
+}
+glm::mat4 Camera::GetProjectionMatrix(float g_width, float g_height) const {
+    return glm::perspective(glm::radians(45.0f), g_width / g_height, 0.1f, 100.0f);
+}
+void Camera::UpdateCameraVectors() {
+    glm::vec3 dir;
+    dir.x = cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+    dir.y = sin(glm::radians(this->pitch));
+    dir.z = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+
+    glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    this->forward = glm::normalize(dir);
+    this->right = glm::normalize(glm::cross(this->forward, worldUp));
+    this->up = glm::normalize(glm::cross(this->right, this->forward));
+}
---
+++ core/camera.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <glm/fwd.hpp>
+#include <glm/vec3.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+
+struct Camera {
+private:
+    glm::vec3 position;
+    glm::vec3 forward;
+    glm::vec3 up;
+    glm::vec3 right;
+    float pitch = 0;
+    float yaw = 0;
+    glm::mat4 view;
+    glm::mat4 projection;
+
+    void UpdateCameraVectors();
+public:
+    // Construct
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f)) {
+        this->position = position;
+
+        this->forward = glm::normalize(target - position);
+        glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+        this->right = glm::normalize(glm::cross(worldUp, this->forward));
+        this->up = glm::cross(this->forward, this->right);
+
+        this->view = glm::lookAt(this->position, this->position + this->forward, this->up);
+    };
+
+    // Copy
+    Camera(const Camera& otherCamera) {
+        this->position = otherCamera.GetPosition();
+        this->forward = otherCamera.GetForward();
+        this->view = otherCamera.GetViewMatrix();
+        this->up = otherCamera.GetUp();
+        this->right = otherCamera.GetRight();
+        this->yaw = otherCamera.GetYaw();
+        this->pitch = otherCamera.GetPitch();
+    }
+
+    // Overload
+    Camera& operator=(const Camera& otherCamera) {
+        if (this != &otherCamera) {
+            this->position = otherCamera.GetPosition();
+            this->forward = otherCamera.GetForward();
+            this->view = otherCamera.GetViewMatrix();
+            this->up = otherCamera.GetUp();
+            this->right = otherCamera.GetRight();
+            this->yaw = otherCamera.GetYaw();
+            this->pitch = otherCamera.GetPitch();
+        }
+        return *this;
+    }
+
+    // Destruct
+    ~Camera() {
+        printf("Camera deleted\n");
+    }
+
+    void MoveCamera(glm::vec3 move);
+    void RotateCamera(glm::vec2 rotation);
+
+    glm::mat4 GetProjectionMatrix(float g_width, float g_height) const;
+    glm::mat4 GetViewMatrix() const { return this->view; }
+    glm::vec3 GetPosition() const { return this->position; }
+    glm::vec3 GetForward() const { return this->forward; }
+    glm::vec3 GetRight() const { return this->right; }
+    glm::vec3 GetUp() const { return this->up; }
+    float GetYaw() const { return this->yaw; }
+    float GetPitch() const { return this->pitch; }
+};
